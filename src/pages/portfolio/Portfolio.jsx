08866f6b76c9e02c25d0dcd5fbce4f5632670639
makersmelx <makersmelx@gmail.com>
2021-12-13 13:12:46 -0500

    feat(*): add clearAll and filter animation

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
-import { Chip, Divider, Fade, Grid, Typography } from '@mui/material';
-import { Computer, Widgets } from '@mui/icons-material';
+import { Chip, Collapse, Divider, Fade, Grid, Typography } from '@mui/material';
+import { Check, Computer, Widgets } from '@mui/icons-material';
 
 import portfolio from '../.././../stuff/portfolio';
 import PortfolioCard from '../../components/PortfolioCard';
@@ -8,6 +8,33 @@ import PortfolioCard from '../../components/PortfolioCard';
 const Index = () => {
   const [tech, setTech] = useState({});
   const [category, setCategory] = useState({});
+
+  const filterFunc = (item, name, stateValue) => {
+    if (item && item.tags && item.tags[name] && item.tags[name]) {
+      for (const key of Object.keys(stateValue)) {
+        if (stateValue[key] && !item.tags[name].find(e => e === key)) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  };
+
+  const Cards = portfolio.filter(item => filterFunc(item, 'tech', tech))
+  .filter(item => filterFunc(item, 'category', category))
+  .map((item) => {
+    const fadeIn = filterFunc(item, 'tech', tech) && filterFunc(item,
+      'category', category);
+    return (
+      <Fade in={fadeIn} appear>
+        <Grid item xs={12} lg={6} key={item.name}>
+          <PortfolioCard item={item} />
+        </Grid>
+      </Fade>
+    );
+  });
+
   const techTagSet = new Set();
   const categoryTagSet = new Set();
   portfolio.forEach(item => {
@@ -25,22 +52,19 @@ const Index = () => {
     }
   });
 
-  const filterFunc = (item, name, stateValue) => {
-    if (item && item.tags && item.tags[name] && item.tags[name]) {
-      for (const key of Object.keys(stateValue)) {
-        if (stateValue[key] && !item.tags[name].find(e => e === key)) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  };
-
   const tagsFromSets = (inputSet, stateValue, setFunc) => {
-    return Array.from(inputSet).sort().map((tag) => (
+    const Tags = Array.from(inputSet).sort().map((tag) => (
       <Grid item>
         <Chip
+          icon={
+            <Collapse
+              style={{ height: '100%' }}
+              orientation="horizontal"
+              in={stateValue && stateValue[tag]}
+            >
+              <Check style={{ marginTop: '20%' }} />
+            </Collapse>
+          }
           label={<Typography sx={{ fontWeight: 500 }}>
             {tag}
           </Typography>}
@@ -57,6 +81,35 @@ const Index = () => {
         />
       </Grid>
     ));
+    const clearAll = () => {
+      setFunc(prev => {
+        return {};
+      });
+    };
+    return (
+      <>
+        {Tags}
+        {Cards.length < portfolio.length && <Grid item xs="auto">
+          <Collapse
+            orientation="horizontal"
+            in={Cards.length < portfolio.length}
+            appear
+          >
+            <Chip
+              label={<Typography sx={{ fontWeight: 500 }}>
+                Clear all
+              </Typography>}
+              key="clear all"
+              color="error"
+              onDelete={clearAll}
+              onClick={clearAll}
+              variant={'outlined'}
+            />
+          </Collapse>
+        </Grid>
+        }
+      </>
+    );
   };
 
   const techTags = useMemo(() => tagsFromSets(techTagSet, tech, setTech),
@@ -65,31 +118,17 @@ const Index = () => {
     () => tagsFromSets(categoryTagSet, category, setCategory),
     [category]);
 
-  const Cards = portfolio.filter(item => filterFunc(item, 'tech', tech))
-  .filter(item => filterFunc(item, 'category', category))
-  .map((item) => {
-    const fadeIn = filterFunc(item, 'tech', tech) && filterFunc(item,
-      'category', category);
-    return (
-      <Fade in={fadeIn} appear>
-        <Grid item xs={12} lg={6} key={item.name}>
-          <PortfolioCard item={item} />
-        </Grid>
-      </Fade>
-    );
-  });
-
   return (
     <>
       <Grid container spacing={1} mt={2} alignItems="center" rowSpacing={1}>
-        <Grid item>
+        <Grid item sx={{ marginRight: 1 }}>
           <Widgets />
         </Grid>
         {categoryTags}
       </Grid>
       <Divider sx={{ mt: 2 }} />
       <Grid container spacing={1} mt={2} alignItems="center" rowSpacing={1}>
-        <Grid item>
+        <Grid item sx={{ marginRight: 1 }}>
           <Computer />
         </Grid>
         {techTags}
